fdeaeeef02f2bb5394c710f2f01123a3bb40492b
KirianCaumes <kirian.caumes@ynov.com>
2019-12-31 10:32:44 +0100

    forgot to add new file

---
+++ api/src/Entity/ParamPayementSolution.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="param_payement_solution")
+ */
+class ParamPayementSolution
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Assert\NotBlank(message = "not_blank")
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @Assert\NotBlank(message = "not_blank")
+     * @ORM\Column(type="string", length=255)
+     */
+    private $icon;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getIcon(): ?string
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(string $icon): self
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+}
