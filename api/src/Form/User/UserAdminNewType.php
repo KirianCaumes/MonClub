bda4812830aa84b1e4c4b3e33931e0023a45c06e
KirianCaumes <kirian.caumes@ynov.com>
2020-02-11 19:21:23 +0100

    create new user, send email

---
+++ api/src/Form/User/UserAdminNewType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form\User;
+
+use App\Entity\Team;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class UserAdminNewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'ROLE_COACH' => 'ROLE_COACH',
+                    'ROLE_ADMIN' => 'ROLE_ADMIN',
+                    'ROLE_SUPER_ADMIN' => 'ROLE_SUPER_ADMIN',
+                ],
+                'multiple' => true
+            ])
+            ->add('teams', EntityType::class, [
+                'class' => Team::class,
+                'multiple' => true
+            ])
+            ->add('enabled', CheckboxType::class)
+            ->add('save', SubmitType::class);
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'csrf_protection' => false,
+            'allow_extra_fields' => true
+        ]);
+    }
+}
