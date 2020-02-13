a6f7622ad31ef9056ab611e8b9ee86ce267426f5
gurvan <gurvan.t@orange.fr>
2020-02-13 19:49:01 +0100

    Email renouvellement certificat gesthand - Bug sur verif date

--- api/src/Service/MailService.php
+++ api/src/Service/MailService.php
@@ -110,4 +110,23 @@ class MailService
                 ]))
         );
     }
+
+    /**
+     * Send an email to remind user to renew the gesthand certificat.
+     */
+    public function sendWarningGesthandCertificat(Member $member,$gesthandCertifDate)
+    {
+        return $this->mailer->send(
+            (new \Swift_Message())
+                ->setContentType('text/html')
+                ->setFrom($this->from)
+                ->setSubject('Renouvellement de votre certificat Gesthand - MonClub THBC')
+                ->setTo($member->getEmail())
+                ->setBody($this->twig->render('/mail/renouvellementCertif.html.twig', [
+                    'user' => $member,
+                    'baseUrl' => $this->baseUrl,
+                    'gesthandCertifDate' =>  $gesthandCertifDate,
+                ]))
+        );
+    }
 }
