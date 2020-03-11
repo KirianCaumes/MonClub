56b6db18915ad0863cdb6a3df142b6a3d36c76b6
KirianCaumes <kirian.caumes@ynov.com>
2020-03-11 16:27:14 +0100

    fix script 2prod

--- 2prod/index.js
+++ 2prod/index.js
@@ -2,9 +2,7 @@ const fs = require('fs')
 const fse = require('fs-extra')
 const rimraf = require("rimraf")
 const envfile = require('envfile')
-const {
-    exec
-} = require('child_process')
+const { exec } = require('child_process')
 const rl = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
@@ -12,35 +10,31 @@ const rl = require("readline").createInterface({
 
 rl.question("Which env (staging/prod) ? ", env => {
     if (!['staging', 'prod'].includes(env)) {
-        console.log("Invalid env")
+        console.error("Invalid env")
         process.exit(1)
     }
     console.log("Starting");
 
     (async () => {
         console.log("Clear build directory")
-        await new Promise((resolve, reject) => rimraf(`${__dirname}/build/`, (err) => { if (err) reject(err); resolve() }))
-        await new Promise((resolve, reject) => fs.mkdir(`${__dirname}/build/`, (err) => { if (err) reject(err); resolve() }))
+        await rm({ path: `${__dirname}/build` })
+        await create({ path: `${__dirname}/build/` })
 
-        await new Promise((resolve, reject) => fs.mkdir(`${__dirname}/build/front`, (err) => { if (err) reject(err); resolve() }))
-        await new Promise((resolve, reject) => fs.mkdir(`${__dirname}/build/api`, (err) => { if (err) reject(err); resolve() }))
-        await new Promise((resolve, reject) => fs.mkdir(`${__dirname}/build/final`, (err) => { if (err) reject(err); resolve() }))
+        await create({ path: `${__dirname}/build/front` })
+        await create({ path: `${__dirname}/build/api` })
+        await create({ path: `${__dirname}/build/final` })
 
         console.log("[FRONT] Copying folders")
-        await fse.copy(`${__dirname}/../front/`, `${__dirname}/build/front`, {
-            filter: name => !name.match(/node_modules/)
-        })
+        await cp({ from: `${__dirname}/../front/`, to: `${__dirname}/build/front`, exclude: /node_modules/ })
 
         console.log("[FRONT] Installing dependencies")
-        await new Promise((resolve, reject) => exec('npm install --silent', { cwd: `${__dirname}/build/front` }, (err, stdout, stderr) => { if (err) reject(err); resolve() }))
+        await run({ command: 'npm install --silent', cwd: `${__dirname}/build/front` })
 
         console.log("[FRONT] Building")
-        await new Promise((resolve, reject) => exec('npm run build --silent', { cwd: `${__dirname}/build/front` }, (err, stdout, stderr) => { if (err) reject(err); resolve() }))
+        await run({ command: 'npm run build --silent', cwd: `${__dirname}/build/front` })
 
         console.log("[API] Copying folders")
-        await fse.copy(`${__dirname}/../api/`, `${__dirname}/build/api`, {
-            filter: name => !name.match(/vendor|cache|log/)
-        })
+        await cp({ from: `${__dirname}/../api/`, to: `${__dirname}/build/api`, exclude: /vendor|cache|log/ })
 
         console.log("[API] Editing .env")
         await new Promise((resolve, reject) => {
@@ -49,6 +43,10 @@ rl.question("Which env (staging/prod) ? ", env => {
             }
             sourceObject.APP_ENV = env
             sourceObject.APP_DEBUG = env !== "prod"
+            sourceObject.JWT_TOKENTTL = 604800
+            
+            delete sourceObject.GMAIL_USERNAME
+            delete sourceObject.GMAIL_PASSWORD
 
             fs.writeFileSync(__dirname + "/build/api/.env", envfile.stringifySync(sourceObject))
             resolve()
@@ -56,15 +54,42 @@ rl.question("Which env (staging/prod) ? ", env => {
 
 
         console.log("[API] Installing dependencies")
-        await new Promise((resolve, reject) => exec(`cd ${__dirname}/build/api`, { cwd: `${__dirname}/build/api` }, (err, stdout, stderr) => { if (err) reject(err); resolve() }))
-        await new Promise((resolve, reject) => exec('composer install --no-dev --optimize-autoloader -q', { cwd: `${__dirname}/build/api` }, (err, stdout, stderr) => { if (err) reject(err); resolve() }))
+        await run({ command: 'composer install --no-dev --optimize-autoloader -q', cwd: `${__dirname}/build/api` })
 
         console.log("Creating final build")
-        await fse.copy(`${__dirname}/build/api/`, `${__dirname}/build/final`)
-        await new Promise((resolve, reject) => rimraf(`${__dirname}/build/final/public/app`, (err) => { if (err) reject(err); resolve() }))
-        await fse.copy(`${__dirname}/build/front/out/`, `${__dirname}/build/final/public/app`)
+        await cp({ from: `${__dirname}/build/api/`, to: `${__dirname}/build/final` })
+        await rm({ path: `${__dirname}/build/final/public/app` })
+        await rm({ path: `${__dirname}/build/final/.env.${env === 'prod' ? 'staging' : 'prod'}` })
+        await cp({ from: `${__dirname}/build/front/build/`, to: `${__dirname}/build/final/public/app` })
 
         console.log("Done")
-
+        process.exit(0)
     })()
 })
+
+function cp({ from = null, to = null, exclude = null }) {
+    return fse.copy(from, to, {
+        filter: name => !name.match(exclude)
+    })
+}
+
+function rm({ path = null }) {
+    return new Promise((resolve, reject) => rimraf(path, (err) => {
+        if (err) reject(err);
+        resolve()
+    }))
+}
+
+function create({ path = null }) {
+    return new Promise((resolve, reject) => fs.mkdir(path, (err) => {
+        if (err) reject(err);
+        resolve()
+    }))
+}
+
+function run({ command = null, cwd = null }) {
+    return new Promise((resolve, reject) => exec(command, { cwd: cwd }, (err, stdout, stderr) => {
+        if (err) reject(err);
+        resolve()
+    }))
+}
--- api/src/Form/Param/ParamReductionFamilyType.php
+++ api/src/Form/Param/ParamReductionFamilyType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Form;
+namespace App\Form\Param;
 
 use App\Entity\ActivityHistory;
 use App\Entity\Document;
